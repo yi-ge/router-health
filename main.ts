6af0af0dc77746563371e645cc4f13d7fe61532d
yi-ge <a@wyr.me>
2021-10-08 02:11:50 +0800

    checkout

--- main.ts
+++ main.ts
@@ -3,8 +3,11 @@ import schedule from 'node-schedule'
 import crypto from 'crypto'
 
 const CRON_CONFIG = '* * * * *' // 检测频率，Cron表达式
-const CHECK_IKUAI = true // 检查爱快
-const CHECK_OPENWRT = true // 检查OpenWRT
+const CHECKOUT_TIMEOUT = 10 * 1000 // 检测超时时间，ms
+const CHECKOUT_TIME = 3 // 检测连续超时x次后重启
+const CHECKOUT_WEB = 'https://www.wyr.me' // 检测基准站点
+const RESTART_IKUAI = true // 重启爱快
+const RESTART_OPENWRT = true // 重启OpenWRT
 
 const IKUAI_ROUTER_SCHEMA = 'http' // 爱快路由器管理地址请求方式，HTTP or HTTPS
 const IKUAI_ROUTER_HOST_AND_PORT = '10.10.11.253' // 爱快路由器管理地址HOST:PORT
@@ -17,11 +20,15 @@ const OPENWRT_USERNAME = 'root'
 const OPENWRT_PASSWORD = ''
 
 const iKuaiPost = bent(IKUAI_ROUTER_SCHEMA + '://' + IKUAI_ROUTER_HOST_AND_PORT, 'POST')
-const openWrtGet = bent(OPENWRT_ROUTER_SCHEMA + '://' + OPENWRT_ROUTER_HOST_AND_PORT, 'GET')
-const openWrtPost = bent(OPENWRT_ROUTER_SCHEMA + '://' + OPENWRT_ROUTER_HOST_AND_PORT, 'POST')
+const openWRTGet = bent(OPENWRT_ROUTER_SCHEMA + '://' + OPENWRT_ROUTER_HOST_AND_PORT, 'GET')
+const openWRTPost = bent(OPENWRT_ROUTER_SCHEMA + '://' + OPENWRT_ROUTER_HOST_AND_PORT, 'POST')
+const checkoutRequest = bent('GET', 200, 201);
+// @ts-ignore
+checkoutRequest.timeout = CHECKOUT_TIMEOUT;
 
 let ikuaiAccessKey: string = ''
-let openWrtAccessKey: string = ''
+let openWRTAccessKey: string = ''
+let timeoutTime = 0
 
 const loginInfo = {
   passwd: crypto.createHash("md5").update(IKUAI_PASSWORD).digest("hex"),
@@ -60,18 +67,14 @@ const restartIkuai = async () => {
   }
 }
 
-// loginIkuai().then(() => {
-//   restartIkuai()
-// })
-
 const loginOpenWRT = async () => {
   try {
-    await openWrtPost('/cgi-bin/luci/', `luci_username=${OPENWRT_USERNAME}&luci_password=${OPENWRT_PASSWORD}`, {
+    await openWRTPost('/cgi-bin/luci/', `luci_username=${OPENWRT_USERNAME}&luci_password=${OPENWRT_PASSWORD}`, {
       'Content-Type': 'application/x-www-form-urlencoded'
     })
   } catch (err: any) {
     if (err && err.statusCode === 302) {
-      openWrtAccessKey = err.headers['set-cookie'][0]
+      openWRTAccessKey = err.headers['set-cookie'][0]
     } else {
       console.log('Username or password incorrect.')
     }
@@ -80,8 +83,8 @@ const loginOpenWRT = async () => {
 
 const restartOpenWRT = async () => {
   try {
-    const getRebootPage: any = await openWrtGet('/cgi-bin/luci/admin/system/reboot', '', {
-      'Cookie': openWrtAccessKey
+    const getRebootPage: any = await openWRTGet('/cgi-bin/luci/admin/system/reboot', '', {
+      'Cookie': openWRTAccessKey
     })
     const rebootPageText = await getRebootPage.text()
     const start = '{ token: \''
@@ -90,9 +93,9 @@ const restartOpenWRT = async () => {
     const token = matchRes ? matchRes[1] : null
     if (!token) console.log('Get token failed.') // 获取重启Token失败
     // console.log(token)
-    const res: any = await openWrtPost('/cgi-bin/luci/admin/system/reboot/call', `token=${token}&_=${Math.random()}`, {
+    const res: any = await openWRTPost('/cgi-bin/luci/admin/system/reboot/call', `token=${token}&_=${Math.random()}`, {
       'Content-Type': 'application/x-www-form-urlencoded',
-      'Cookie': openWrtAccessKey
+      'Cookie': openWRTAccessKey
     })
 
     if (res && res.statusCode === 200) {
@@ -103,11 +106,33 @@ const restartOpenWRT = async () => {
   }
 }
 
-// loginOpenWRT().then(() => {
-//   restartOpenWRT()
-// })
+const checkoutNetwork = async () => {
+  try {
+    const res: any = await checkoutRequest(CHECKOUT_WEB)
+    if (res.StatusCode === 200) {
+      timeoutTime = 0
+    }
+  } catch (err: any) {
+    console.log(err)
+    // if (err.code) {
+    // }
+    if (++timeoutTime + 1 === CHECKOUT_TIME) {
+      if (RESTART_IKUAI) {
+        loginIkuai().then(() => {
+          restartIkuai()
+        })
+      }
+      if (RESTART_OPENWRT) {
+        loginOpenWRT().then(() => {
+          restartOpenWRT()
+        })
+      }
+    }
+  }
+}
 
-const job = schedule.scheduleJob('* * * * *', function () {
-  console.log('The answer to life, the universe, and everything!');
+checkoutNetwork()
+
+const job = schedule.scheduleJob(CRON_CONFIG, function () {
+  checkoutNetwork()
 })
-
